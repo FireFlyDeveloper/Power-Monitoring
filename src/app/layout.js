66994a2a4a4bf7e073b20c0799c2586e53255877
Kim <saludeskimdev@gmail.com>
2025-09-06 20:37:52 +0800

    change title

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,8 +13,8 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata = {
-  title: "Dashboard",
-  description: "Dashboard",
+  title: "Introduction",
+  description: "Introduction",
 };
 
 export default function RootLayout({ children }) {
