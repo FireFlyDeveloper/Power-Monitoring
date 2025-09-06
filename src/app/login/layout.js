0ca154b061b86c24d607793e781e9edeb0a40d04
Kim <saludeskimdev@gmail.com>
2025-09-06 10:44:05 +0800

    bug fix

--- src/app/login/layout.js
+++ src/app/login/layout.js
@@ -1,4 +1,15 @@
+import { Geist, Geist_Mono } from "next/font/google";
 import "../globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
 
 export const metadata = {
   title: "Login",
@@ -7,10 +18,12 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body>
-        {children}
-      </body>
-    </html>
+      <html lang="en">
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          {children}
+        </body>
+      </html>
   );
 }
